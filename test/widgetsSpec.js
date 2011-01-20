lemmea7/angular.js

commit 342eb56adf0891aff0a1f35452240cc3654665b3

$route should create child scope via $new

src/widgets.js
@@ -625,20 +625,9 @@ angularWidget('ng:include', function(element){
     return extend(function(xhr, element){
       var scope = this, childScope;
       var changeCounter = 0;
-      var preventRecursion = false;
       function incrementChange(){ changeCounter++;}
       this.$watch(srcExp, incrementChange);
       this.$watch(scopeExp, incrementChange);
-      scope.$onEval(function(){
-        if (childScope && !preventRecursion) {
-          preventRecursion = true;
-          try {
-            childScope.$eval();
-          } finally {
-            preventRecursion = false;
-          }
-        }
-      });
       this.$watch(function(){return changeCounter;}, function(){
         var src = this.$eval(srcExp),
             useScope = this.$eval(scopeExp);

test/widgetsSpec.js
@@ -651,7 +651,7 @@ describe("widget", function(){
 
       // this one should really be just '1', but due to lack of real events things are not working
       // properly. see discussion at: http://is.gd/ighKk
-      expect(element.text()).toEqual('4');
+      expect(element.text()).toEqual('2');
       dealoc(element);
     });
 

src/services.js
@@ -708,7 +708,7 @@ angularServiceInject('$route', function(location) {
       if (!childScope) {
         var pathParams = matcher(location.hashPath, route);
         if (pathParams) {
-          childScope = createScope(parentScope);
+          childScope = parentScope.$new();
           $route.current = extend({}, routeParams, {
             scope: childScope,
             params: extend({}, location.hashSearch, pathParams)

commit 61c6a0beb738d05d0ab8cdd71d91688e54701edc

fix ng:include issue introduced by 342eb56a

src/widgets.js
@@ -635,7 +635,12 @@ angularWidget('ng:include', function(element){
         if (src) {
           xhr('GET', src, function(code, response){
             element.html(response);
-            childScope = useScope || createScope(scope);
+            if (useScope) {
+              childScope = useScope;
+            } else {
+              childScope = createScope(scope);
+              scope.$onEval(childScope.$eval);
+            }
             compiler.compile(element)(element, childScope);
             childScope.$init();
             scope.$eval(onloadExp);

test/widgetsSpec.js
@@ -609,7 +609,24 @@ describe("widget", function(){
   });
 
   describe('ng:include', function(){
-    it('should include on external file', function() {
+    it('should include on external file and create a new child scope', function() {
+      var element = jqLite('<ng:include src="url"></ng:include>');
+      var scope = angular.compile(element);
+      scope.counter = 0;
+      scope.url = 'myUrl';
+      scope.$service('$xhr.cache').data.myUrl = {value:'{{counter = counter + 1}}'};
+      scope.$init();
+      scope.$service('$browser').defer.flush();
+      expect(element.text()).toEqual('2');
+
+      //should also propagate evals to the child scope
+      scope.$eval();
+      expect(element.text()).toEqual('3');
+
+      dealoc(scope);
+    });
+
+    it('should include on external file and use an existing child scope', function() {
       var element = jqLite('<ng:include src="url" scope="childScope"></ng:include>');
       var scope = angular.compile(element);
       scope.childScope = createScope();